fc22b8dfa3f0897c8020a410a82356ed609ba591
VCG-cphillips <47870744+VCG-cphillips@users.noreply.github.com>
2019-08-02 20:27:31 -0500

    Finished Lab 1

--- PS-Lab1.ps1
+++ PS-Lab1.ps1
@@ -27,3 +27,25 @@ mkdir LabOutput
 
 # Copy WindowsDir into LabOutput
 Copy-Item -Path C:\Windows\WindowsDir.txt -Destination LabOutput\WindowsDir.txt
+copy C:\Windows\WindowsDir.txt LabOutput\WindowsDir.txt
+
+# Delete original WindowsDir.txt file
+Remove-Item -Path C:\Windows\WindowsDir.txt
+del C:\Windows\WindowsDir.txt
+
+# Display List of Running Properties
+Get-Process
+tasklist
+
+# Redirect Processes to Procs.txt
+Get-Process > C:\Windows\Procs.txt
+tasklist > C:\Windows\Procs.txt
+
+# Move Procs.txt to LabOutput Folder
+Copy-Item -Path C:\Windows\Procs.txt -Destination C:\Windows\LabOutput\Procs.txt
+copy C:\Windows\Procs.txt C:\Windows\LabOutput\Procs.txt
+
+# Display Content from newly pasted file a page at a time
+Get-Content C:\Windows\LabOutput\Procs.txt | more
+type C:\Windows\LabOutput\Procs.txt | more
+
